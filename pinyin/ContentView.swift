6801c0a2eddf2ea105d5676d9b7cbfa4cccbd0ff
旺宝网络 <108932467@qq.com>
2023-10-04 13:59:18 +0800

    Initial Commit

---
+++ pinyin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pinyin
+//
+//  Created by 黄文琳 on 2023/10/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world1!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pinyin/pinyinApp.swift
@@ -0,0 +1,17 @@
+//
+//  pinyinApp.swift
+//  pinyin
+//
+//  Created by 黄文琳 on 2023/10/4.
+//
+
+import SwiftUI
+
+@main
+struct pinyinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
